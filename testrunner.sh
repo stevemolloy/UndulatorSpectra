5437be83407201b5c262615d0d96f67ded98c441
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2019-02-14 11:17:58 +0100

    mypy should be flagged for python2 when appropriate

--- testrunner.sh
+++ testrunner.sh
@@ -1,2 +1,12 @@
 python -m unittest discover -s tests/
-mypy tests
+
+MYVAR=`python -c 'import sys; print(sys.version_info[0])'`
+if (( $MYVAR == 2 ))
+	then
+		echo "Using Python2"
+		mypy --py2 tests
+elif (( $MYVAR == 3 ))
+	then
+		echo "Using Python3"
+		mypy tests
+fi
